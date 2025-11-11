0f2340c304907f4ff7694ece738d58813c885f03
Oscar Alberto Garcia Lopez <racso80@gmail.com>
2025-11-11 18:15:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5995013944-ec4f8",
+  "appId": "1:1011296435123:web:a4a9c1f622f6c843b5a9c6",
+  "apiKey": "AIzaSyDaE6Bi9zNiKk6DK9j4r0CNeaU0TAsuTqA",
+  "authDomain": "studio-5995013944-ec4f8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1011296435123"
+};
