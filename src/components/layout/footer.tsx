8e5f8b4351a32ce853a0531801b342fec812caf2
Oscar Alberto Garcia Lopez <racso80@gmail.com>
2025-12-16 15:57:20 +0000

    modifica este texto: © 2025 CitaMedicaFacil. y coloca © 2025 CitaMedicaF

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ export function SiteFooter() {
     <footer className="py-6 md:px-8 md:py-0 border-t">
       <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
-          © {new Date().getFullYear()} CitaMedicaFacil.
+          © {new Date().getFullYear()} CitaMedicaFacil. Powered By Ing. Oscar Alberto García López GL Systems
         </p>
       </div>
     </footer>
